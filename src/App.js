82a1d582ea69c84040b3f54533181ce08e4f65c6
Hugo Danet <61951279+cartesis07@users.noreply.github.com>
2023-02-26 16:30:18 +0100

    add data and remove useless text

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { data, keys } from "./data";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
       <div className="responsive-chord">
         <MyResponsiveChord data={data} keys={keys} />
       </div>
