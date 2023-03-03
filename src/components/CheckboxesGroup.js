e072a6c0c30edb6b593b6362451b24f367c44b1f
Hugo Danet <61951279+cartesis07@users.noreply.github.com>
2023-03-03 09:27:52 +0000

    pokemon types logos

--- src/components/CheckboxesGroup.js
+++ src/components/CheckboxesGroup.js
@@ -1,14 +1,53 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import FormLabel from "@mui/material/FormLabel";
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import FormHelperText from "@mui/material/FormHelperText";
 import Checkbox from "@mui/material/Checkbox";
 import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
 import { MyResponsiveChord } from "./MyResponsiveChord";
 
+import bug from "../images/Bug.png";
+import dark from "../images/Dark.png";
+import dragon from "../images/Dragon.png";
+import electric from "../images/Electric.png";
+import fairy from "../images/Fairy.png";
+import fighting from "../images/Fighting.png";
+import fire from "../images/Fire.png";
+import flying from "../images/Flying.png";
+import ghost from "../images/Ghost.png";
+import grass from "../images/Grass.png";
+import ground from "../images/Ground.png";
+import ice from "../images/Ice.png";
+import normal from "../images/Normal.png";
+import poison from "../images/Poison.png";
+import psychic from "../images/Psychic.png";
+import rock from "../images/Rock.png";
+import steel from "../images/Steel.png";
+import water from "../images/Water.png";
+
 export default function CheckboxesGroup({ data, keys }) {
+  const imagesArray = [
+    bug,
+    dark,
+    dragon,
+    electric,
+    fairy,
+    fighting,
+    fire,
+    flying,
+    ghost,
+    grass,
+    ground,
+    ice,
+    normal,
+    poison,
+    psychic,
+    rock,
+    steel,
+    water,
+  ];
+
   const [state, setState] = React.useState({
     type_selector: new Array(keys.length).fill(true),
     chord_data: data,
@@ -34,13 +73,11 @@ export default function CheckboxesGroup({ data, keys }) {
       }
     }
 
-    console.log(new_chord_data);
-
     setState({
       ...state,
       type_selector: new_type_selector,
-      chord_data: new_chord_data,
       chord_keys: new_chord_keys,
+      chord_data: new_chord_data,
     });
   };
 
@@ -62,6 +99,19 @@ export default function CheckboxesGroup({ data, keys }) {
               key={index}
               control={
                 <Checkbox
+                  icon={
+                    <Avatar
+                      sx={{ width: 20, height: 20 }}
+                      src={imagesArray[index]}
+                      style={{ filter: "brightness(50%)" }}
+                    ></Avatar>
+                  }
+                  checkedIcon={
+                    <Avatar
+                      sx={{ width: 20, height: 20 }}
+                      src={imagesArray[index]}
+                    ></Avatar>
+                  }
                   checked={state.type_selector[index]}
                   name={keys[index]}
                   onChange={(e) => handleChange(e, index)}
@@ -69,9 +119,11 @@ export default function CheckboxesGroup({ data, keys }) {
                 />
               }
               label={
-                <Typography variant="body2" color="textSecondary">
-                  {name}
-                </Typography>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <Typography variant="body2" color="textSecondary">
+                    {name}
+                  </Typography>
+                </div>
               }
             />
           );
